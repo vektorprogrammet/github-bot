3757af1168f2badc0a0427acdcfdac3b651f7780
Kristoffer <kra1992@hotmail.com>
2018-04-04 09:40:06 +0200

    Better error handling

--- main.go
+++ main.go
@@ -48,91 +48,170 @@ func (s *GitHubEventMonitor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func doBotStuff(event *github.PullRequestEvent) {
 	branch := *event.PullRequest.Head.Ref
-	repoFolderName := cloneRepo(*event.Repo.SSHURL)
+	repoFolderName, err := cloneRepo(*event.Repo.SSHURL)
+	if err != nil {
+		fmt.Println("Failed to clone repository " + *event.Repo.SSHURL)
+		return
+	}
 	defer cleanUp(repoFolderName)
 
-	createParametersFile(repoFolderName)
-	installDependencies()
-	createDatabase()
-	checkout(branch)
-	executeMigrations()
-	createMigration()
-	fixCodeStyle()
-	gitPush()
+	if err := createParametersFile(repoFolderName); err != nil {
+		fmt.Println("Failed to create parameters file")
+		return
+	}
+
+	if err := installDependencies(); err != nil {
+		fmt.Println("Failed to install dependencies")
+		return
+	}
+	if err := createDatabase(); err != nil {
+		fmt.Println("Failed to create database")
+		return
+	}
+	if err := checkout(branch); err != nil {
+		fmt.Println("Failed checkout " + branch)
+		return
+	}
+	if err := installDependencies(); err != nil {
+		fmt.Println("Failed to install dependencies")
+		return
+	}
+	if err := executeMigrations(); err != nil {
+		fmt.Println("Failed to execute migrations")
+		return
+	}
+	if err := createMigration(); err != nil {
+		fmt.Println("Failed to create migration")
+		return
+	}
+	if err := fixCodeStyle(); err != nil {
+		fmt.Println("Failed to fix code style")
+		return
+	}
+	if err := gitPush(); err != nil {
+		fmt.Println("Failed to git push")
+		return
+	}
 }
 
-func cloneRepo(repo string) string {
+func cloneRepo(repo string) (string, error) {
 	repoFolderName := fmt.Sprintf("%s", uuid.NewV4())
-	executeCommand(fmt.Sprintf("git clone %s %s/%s", repo, rootDir, repoFolderName))
-	cd(fmt.Sprintf("%s/%s", rootDir, repoFolderName))
+	if _, err := executeCommand(fmt.Sprintf("git clone %s %s/%s", repo, rootDir, repoFolderName)); err != nil {
+		return "", err
+	}
 
-	return repoFolderName
+	if err := cd(fmt.Sprintf("%s/%s", rootDir, repoFolderName)); err != nil {
+		return "", err
+	}
+
+	return repoFolderName, nil
 }
 
-func installDependencies() {
-	executeCommand("composer install -n")
+func installDependencies() error {
+	_, err := executeCommand("composer install -n")
+	return err
 }
 
-func createParametersFile(dbName string) {
-	executeCommand(fmt.Sprintf("cp %s/parameters.yml app/config/parameters.yml", rootDir))
-	executeCommand(fmt.Sprintf("sed -i 's/dbname/%s/g' app/config/parameters.yml", dbName))
+func createParametersFile(dbName string) error {
+	if _, err := executeCommand(fmt.Sprintf("cp %s/parameters.yml app/config/parameters.yml", rootDir)); err != nil {
+		return err
+	}
+	_, err := executeCommand(fmt.Sprintf("sed -i 's/dbname/%s/g' app/config/parameters.yml", dbName))
+	return err
 }
 
-func createDatabase() {
-	executeCommand("php app/console doctrine:database:create")
-	executeCommand("php app/console doctrine:schema:create")
-	executeCommand("php app/console doctrine:migrations:version --add --all -n")
+func createDatabase() error {
+	if _, err := executeCommand("php app/console doctrine:database:create"); err != nil {
+		return err
+	}
+	if _, err := executeCommand("php app/console doctrine:schema:create"); err != nil {
+		return err
+	}
+	_, err := executeCommand("php app/console doctrine:migrations:version --add --all -n")
+	return err
 }
 
-func checkout(branch string) {
-	executeCommand(fmt.Sprintf("git checkout %s", branch))
+func checkout(branch string) error {
+	_, err := executeCommand(fmt.Sprintf("git checkout %s", branch))
+	return err
 }
 
-func executeMigrations() {
-	executeCommand("php app/console doctrine:migrations:migrate -n")
+func executeMigrations() error {
+	_, err := executeCommand("php app/console doctrine:migrations:migrate -n")
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func createMigration() {
-	migrationResult := executeCommand("php app/console doctrine:migrations:diff")
+func createMigration() error {
+	migrationResult, err := executeCommand("php app/console doctrine:migrations:diff")
+	if err != nil {
+		return err
+	}
+
 	if strings.Contains(migrationResult, "Generated new migration") {
-		executeCommand("git add app/DoctrineMigrations/")
-		executeCommand("git commit -m 'Create database migration'")
+		_, err = executeCommand("git add app/DoctrineMigrations/")
+		if err != nil {
+			return err
+		}
+
+		_, err = executeCommand("git commit -m 'Create database migration'")
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
-func fixCodeStyle() {
-	codeStyleResult := executeCommand("npm run cs")
+func fixCodeStyle() error {
+	codeStyleResult, err := executeCommand("npm run cs")
+	if err != nil {
+		return err
+	}
 	if strings.Contains(codeStyleResult, "1)") {
-		executeCommand("git add src/")
-		executeCommand("git commit -m 'Fix code style'")
+		if _, err = executeCommand("git add src/"); err != nil {
+			return err
+		}
+		if _, err = executeCommand("git commit -m 'Fix code style'"); err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
-func gitPush() {
-	executeCommand("git push")
+func gitPush() error {
+	_, err := executeCommand("git push")
+	return err
 }
 
-func cleanUp(folderName string) {
-	executeCommand("php app/console doctrine:database:drop --force")
+func cleanUp(folderName string) error {
+	if _, err := executeCommand("php app/console doctrine:database:drop --force"); err != nil {
+		return err
+	}
 	if len(folderName) > 1 {
-		executeCommand(fmt.Sprintf("rm -rf %s/%s/", rootDir, folderName))
+		if _, err := executeCommand(fmt.Sprintf("rm -rf %s/%s/", rootDir, folderName)); err != nil {
+			return err
+		}
 	}
-	cd(rootDir)
+	return cd(rootDir)
 }
 
-func executeCommand(command string) string {
+func executeCommand(command string) (string, error) {
 	cmd := exec.Command("sh", "-c", command)
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Failed to execute " + command)
-		log.Fatal(err)
+		return "", err
 	}
 
-	return fmt.Sprintf("%s", output)
+	return fmt.Sprintf("%s", output), nil
 }
 
-func cd(dir string) {
-	os.Chdir(dir)
+func cd(dir string) error {
+	return os.Chdir(dir)
 }
 
 func main() {
